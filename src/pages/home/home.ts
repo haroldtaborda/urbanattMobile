0a4ff68f0c857820220c81161604e2d63d945e43
haroldtaborda <haroldtaborda93@gmail.com>
2018-09-04 12:23:37 -0500

    commit2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { NgModule } from '@angular/core';
-import { TabsPage } from '../tabs/tabs';
-import { InicioPage } from '../inicio/inicio';
-import { ActivatedRoute } from '@angular/router'; 
-import { Router  } from '@angular/router';
-
-
 
 @Component({
   selector: 'page-home',
@@ -14,24 +7,8 @@ import { Router  } from '@angular/router';
 })
 export class HomePage {
 
-  
   constructor(public navCtrl: NavController) {
-  
+
   }
-  
-  private iniciar(_credentials) {
-      console.log("Inicia sesion");
-      }
-     
-     
-private crear(_credentials) {
-    console.log("crea usuario");
-      
-      }
-      
-      private recordar(_credentials) {
-    console.log("Olvido contraseña");
-      
-      }
 
 }
