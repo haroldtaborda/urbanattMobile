608f8d311146ac4bb7c6bcd3d8864a841eca0bb5
haroldtaborda <haroldtaborda93@gmail.com>
2018-09-07 08:58:44 -0500

    Crud clientes

--- src/pages/clientes/clientes.ts
+++ src/pages/clientes/clientes.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { App, MenuController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AlertController } from 'ionic-angular';
+
 @Component({
   selector: 'page-clientes',
   templateUrl: 'clientes.html'
@@ -33,9 +35,64 @@ export class ClientesPage {
     {tipo : 'CC', numero: 1094937634, nombre: 'Harold Steven Taborda Gutierrez', telefono : 3218818879, direccion: 'Mirador de'}
   ];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
   }
 
   
+  eliminarCliente(res){
+    const index: number = this.ResArray.indexOf(res);
+    if (index !== -1) {
+        this.ResArray.splice(index, 1);
+        this.mensajeConfirmacion('Exito', 'Registro eliminado exitosamente ');
+    }   
+  }
+
+  create(){
+
+  }
+  edit(){
+
+  }
+  details(){
+    
+  }
+
+  //MODALS
+  mensajeConfirmacion(titulo, mensaje){
+    let alert = this.alertCtrl.create({
+      title: titulo,
+      subTitle: mensaje,
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+  
+  modalConfirm(res){
+    let alert = this.alertCtrl.create({
+      title: 'Confirmación',
+      message: 'Al realizar esta acción se perderan los datos eliminados',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            this.eliminarCliente(res);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+
+
+
 }
 
