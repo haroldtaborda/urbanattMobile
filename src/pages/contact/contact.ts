4511e09907b4d1fbdeb44e929826a881397de3df
Edwin <earo1228@gmail.com>
2018-09-07 20:18:22 -0500

    Comit inicial de contactenos
    Se realiza commit inicial de la pagina contactenos con su logica basica.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -32,8 +32,21 @@ export class ContactPage {
     {tipo : 'CC', numero: 1094937634, nombre: 'Harold Steven Taborda Gutierrez', telefono : 3218818879, direccion: 'Mirador de'}
   ];
 
+  private paso:number =1;
+  private mensaje:string ="Siguiente";
   constructor(public navCtrl: NavController) {
 
+  }
+
+  private siguiente(){
+    this.paso++;
+    if(this.paso==3){
+      this.mensaje="Enviar";
+    }
+    if(this.paso==4){
+      this.paso=1;
+    }
+    
   }
 
 }
